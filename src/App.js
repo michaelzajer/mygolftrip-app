05d536346423c84cd1f85b2dc780b181a94db23b
Michael Zajer <michael.zajer@gmail.com>
2023-10-22 16:52:59 +1100

    created header and links

--- src/App.js
+++ src/App.js
@@ -5,16 +5,22 @@ import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import ForgotPassword from "./pages/ForgotPassword"
 import Offers from "./pages/Offers"
+import Header from "./components/Header";
+import Mytrips from "./pages/Mytrips";
+import Leaderboard from "./pages/Leaderboard";
 
 function App() {
   return (
     <>
       <Router>
+        <Header/>
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/profile" element={<Profile/>} />
           <Route path="/sign-in" element={<SignIn/>} />
           <Route path="/sign-up" element={<SignUp/>} />
+          <Route path="/mytrips" element={<Mytrips/>} />
+          <Route path="/leaderboard" element={<Leaderboard />} />
           <Route path="/ForgotPassword" element={<ForgotPassword/>} />
           <Route path="/offers" element={<Offers/>} />
         </Routes>
---
+++ src/components/Header.jsx
@@ -0,0 +1,56 @@
+import { useLocation, useNavigate } from "react-router-dom"
+export default function Header() {
+    const location = useLocation()
+    const navigate = useNavigate()
+    function pathMatchRoute(route){
+        if(route === location.pathname){
+            return true
+        }
+    }
+  return (
+    <div className='bg-white border-b shadow-sm sticky top-o z-50'>
+        <header className='flex justify-between items-center
+        px-3 max-w-6xl mx-auto'>
+            <div>
+                <img src="https://golf-trip-v1-storage-7ccb4a8094550-staging.s3.amazonaws.com/public/mygolftrip_logo.png"
+                alt="logo"
+                className="h-8 cursor-pointer" />
+            </div>
+            <div>
+                <ul className='flex space-x-10'>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold
+                        text-grey-400 border-b-[3px]
+                        border-b-transparent
+                        ${pathMatchRoute("/") && "text-black border-b-red-500"}`}
+                        onClick={()=>navigate("/")} 
+                        >Home</li>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold
+                        text-grey-400 border-b-[3px]
+                        border-b-transparent
+                        ${pathMatchRoute("/mytrips") && "text-black border-b-red-500"}`}
+                        onClick={()=>navigate("/mytrips")} 
+                        >MyTrips</li>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold
+                        text-grey-400 border-b-[3px]
+                        border-b-transparent
+                        ${pathMatchRoute("/leaderboard") && "text-black border-b-red-500"}`}
+                        onClick={()=>navigate("/leaderboard")} 
+                        >Leaderboard</li>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold
+                        text-grey-400 border-b-[3px]
+                        border-b-transparent
+                        ${pathMatchRoute("/admin") && "text-black border-b-red-500"}`}
+                        onClick={()=>navigate("/admin")} 
+                        >Admin</li>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold
+                        text-grey-400 border-b-[3px]
+                        border-b-transparent
+                        ${pathMatchRoute("/sign-in") && "text-black border-b-red-500"}`}
+                        onClick={()=>navigate("/sign-in")} 
+                        >Sign In</li>
+                </ul>
+            </div>
+        </header>
+    </div>
+  )
+}
